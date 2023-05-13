d8e89cb72264816fffff35cc2821bbf41dbfe7f9
bitxzibit3 <bitxzibit3@gmail.com>
2023-05-13 11:13:52 +0300

    Edited list template type to std::vector

--- lab_03/src/hanoi.cpp
+++ lab_03/src/hanoi.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include "List.cpp"
+#include <vector>
 
-void move (int disk_amount, int from, int to, int temp, List<int> &list) {
+void move (int disk_amount, int from, int to, int temp, List<std::vector<int> > &list) {
     if (disk_amount == 0)
         return;
     move(disk_amount - 1, from, temp, to, list);
-
-    list.add_node(from);
-    list.add_node(to);
+    std::vector<int> tmp {from, to};
+    list.add_node(tmp);
     move(disk_amount - 1, temp, to, from, list);
 }
 
-void print_info(List<int> list) {
+void print_info(List<std::vector<int> > list) {
     auto cur = list.get_root();
     while(cur != nullptr) {
-        int from = cur->get_value();
-        cur = cur->get_next();
-        int to = cur->get_value();
+        auto values = cur->get_value();
+        int from = values[0];
+        int to = values[1];
         std::cout << "From " << from << " to " << to << std::endl << std::flush;
         cur = cur->get_next();
     }
@@ -24,7 +24,7 @@ void print_info(List<int> list) {
 
 int main() {
     int DISKS_COUNT = 8;
-    List<int> list;
+    List<std::vector<int> > list;
     move(DISKS_COUNT, 1,2,3, list);
     print_info(list);
     list.DeleteLinkedList();
