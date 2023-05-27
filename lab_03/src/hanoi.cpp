dd2dbd167f30e4ec5a59aee259b9e4e38d5f3348
bitxzibit3 <bitxzibit3@gmail.com>
2023-05-27 09:23:53 +0300

    Edited template type in hanoi from std::vector to std::pair

--- lab_03/src/hanoi.cpp
+++ lab_03/src/hanoi.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 #include "List.cpp"
-#include <vector>
 
-void move (int disk_amount, int from, int to, int temp, List<std::vector<int> > &list) {
+void move (int disk_amount, int from, int to, int temp, List<std::pair<int, int> > &list) {
     if (disk_amount == 0)
         return;
     move(disk_amount - 1, from, temp, to, list);
-    std::vector<int> tmp {from, to};
+    std::pair<int, int> tmp (from, to);
     list.add_node(tmp);
     move(disk_amount - 1, temp, to, from, list);
 }
 
-void print_info(List<std::vector<int> > list) {
+void print_info(List<std::pair<int, int> > list) {
     auto cur = list.get_root();
+    int iter_counter = 0;
     while(cur != nullptr) {
         auto values = cur->get_value();
-        int from = values[0];
-        int to = values[1];
-        std::cout << "From " << from << " to " << to << std::endl << std::flush;
+        int from = values.first;
+        int to = values.second;
+        std::cout << "From " << from << " to " << to << std::endl;
         cur = cur->get_next();
+        iter_counter++;
     }
+    std::cout << "Iterations count: " << iter_counter << std::endl;
 }
 
 int main() {
     int DISKS_COUNT = 8;
-    List<std::vector<int> > list;
+    List<std::pair<int, int> > list;
     move(DISKS_COUNT, 1,2,3, list);
     print_info(list);
     list.DeleteLinkedList();
