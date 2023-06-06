3f79f0ba4fa03a27c5787a97c387617e1d561271
bitxzibit3 <bitxzibit3@gmail.com>
2023-06-06 14:22:25 +0300

    Lab5 done

---
+++ lab_05/src/FileHandler/FileHandler.cpp
@@ -0,0 +1,50 @@
+//
+// Created by timur on 02.06.23.
+//
+
+#include "FileHandler.h"
+
+FileHandler::FileHandler() {
+    this->path = "out.txt";
+}
+
+FileHandler::FileHandler(const std::string& p) {
+    this->path = p;
+}
+
+int FileHandler::write(std::string s) {
+    std::ofstream fout;
+    fout.open(this->path, std::ofstream::app);
+    if (!fout.is_open()) {
+        std::cout << "File can not be opened\n";
+        return -1;
+    }
+    fout << s << std::endl;
+    fout.close();
+    return 0;
+}
+
+int FileHandler::write(std::vector<std::string> v) {
+    std::ofstream fout;
+    fout.open(this->path, std::ofstream::app);
+    if (!fout.is_open()) {
+        std::cout << "File can not be opened\n";
+        return -1;
+    }
+    for (auto s: v)
+        fout << s << std::endl;
+    fout.close();
+    return 0;
+}
+
+int FileHandler::write(float num) {
+    std::ofstream fout;
+    fout.open(this->path, std::ofstream::app);
+    if (!fout.is_open()) {
+        std::cout << "File can not be opened\n";
+        return -1;
+    }
+    fout << num << std::endl;
+    fout.close();
+    return 0;
+}
